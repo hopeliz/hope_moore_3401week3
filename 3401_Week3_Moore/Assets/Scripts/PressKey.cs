d5667f355280a7cc3b26fd808a0e48320bc1ceea
Hope Moore <hopemoore@gmail.com>
2019-02-05 12:29:11 -0500

    Uploading project

---
+++ 3401_Week3_Moore/Assets/Scripts/PressKey.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PressKey : MonoBehaviour {
+
+    public Animator anim;
+    public AudioSource audioToPlay;
+    public Slider volume;
+
+    void Update()
+    {
+        audioToPlay.volume = volume.value;
+    }
+
+    public void PressDown()
+    {
+        anim.SetTrigger("press");
+        anim.Play("KeyUp");
+        audioToPlay.Play();
+        print("Clicked!");
+    }
+}
---
+++ 3401_Week3_Moore/Assets/Scripts/TitleVolumeControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleVolumeControl : MonoBehaviour {
+
+    public AudioSource audioToPlay;
+    public Slider volume;
+	
+	// Update is called once per frame
+	void Update () {
+        audioToPlay.volume = volume.value;
+	}
+}
